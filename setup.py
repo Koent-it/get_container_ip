6191faa9fbfa13f9e10d2555670fce8841c74a9d
Davide Quattrocchi <davide.quattrocchi@koent.it>
2025-01-17 16:02:35 +0100

    fix(build): fix pyproject.toml

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
   name='get_container_ip',
-  version='0.1.0',
   packages=find_packages(include=['get_container_ip', 'get_container_ip.*']),
   install_requires=[
     'argparse',
@@ -14,10 +13,4 @@ setup(
   long_description=open('README.md').read(),
   long_description_content_type='text/markdown',
   url='https://github.com/Koent-it/get_container_ip',
-  classifiers=[
-    'Programming Language :: Python :: 3',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent',
-  ],
-  python_requires='>=3.6',
 )
